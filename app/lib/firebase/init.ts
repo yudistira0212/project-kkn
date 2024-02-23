0d887c40db3c9f9b92cc38898cc1d92d6811cd62
Akmal Yudistira <akmalyudistira1212@gmail.com>
2024-02-23 11:03:16 +0900

    fire base

--- app/lib/firebase/init.ts
+++ app/lib/firebase/init.ts
@@ -6,7 +6,23 @@ import { initializeApp } from "firebase/app";
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {};
+const firebaseConfig = {
+  apiKey: "AIzaSyBCICPtbMg_Cv705ZNM_FP5pk-ft9hISjs",
+  authDomain: "kampung-siwi.firebaseapp.com",
+  projectId: "kampung-siwi",
+  storageBucket: "kampung-siwi.appspot.com",
+  messagingSenderId: "837815733103",
+  appId: "1:837815733103:web:a8383d4667eaf65f22769a",
+  measurementId: "G-L48GGJ314M",
+
+  // apiKey: process.env.FIREBASE_API_KEY,
+  // authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  // projectId: process.env.FIREBASE_PROJECT_ID,
+  // storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  // messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+  // appId: process.env.FIREBASE_APP_ID,
+  // measurementId: process.env.FIREBASE_MEASUREMENT_ID,
+};
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
