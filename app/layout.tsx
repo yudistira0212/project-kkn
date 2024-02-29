e013bd9610bff39204f7ef878e406ff0c86de6b8
Akmal Yudistira <akmalyudistira1212@gmail.com>
2024-02-29 11:59:22 +0900

    ganti favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -10,6 +10,9 @@ const inter = Inter({ subsets: ["latin"] });
 export const metadata: Metadata = {
   title: "Kampung Siwi",
   description: "Kampung Siwi",
+  icons: {
+    icon: "https://firebasestorage.googleapis.com/v0/b/kampung-siwi.appspot.com/o/logo%2F640px-Lambang_Kabupaten_Manokwari_Selatan.png?alt=media&token=c821a500-d955-4020-96af-069d5e12c863",
+  },
 };
 
 export default function RootLayout({
